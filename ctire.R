050ce02d9b59445413d329361d907d68a57ad364
Cameron <cwillmot@gmail.com>
2021-08-30 15:57:29 -0400

    create scrape_canadian_tire function for future use with lists

--- ctire.R
+++ ctire.R
@@ -17,11 +17,10 @@ package.check <- lapply(
 # Source the saving script for later use
 source('dbsave.R')
 
-product_url <- "https://www.canadiantire.ca/en/pdp/napoleon-ld3-3-burner-propane-grill-0853158p.html#srp"
+# No longer needed product_url <- "https://www.canadiantire.ca/en/pdp/napoleon-ld3-3-burner-propane-grill-0853158p.html#srp"
 
 scrape_url <- function(url) {
   system(paste("C:\\Users\\Cam\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe ctscrape.js",url))
-  
   product_html <- read_html('ct.html')
   
   product_price <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "price__now--value", " " ))]') %>% html_text()
@@ -44,8 +43,26 @@ scrape_url <- function(url) {
     request_url = url,
     date_scraped = Sys.time()
   )
-  
   save_scrape(product) 
 }
 
-scrape_url(product_url)
+product_url_list <- c(
+  "https://www.canadiantire.ca/en/pdp/napoleon-ld4-4-burner-natural-gas-grill-0853161p.html#srp",
+  "https://www.canadiantire.ca/en/pdp/napoleon-ld3-3-burner-propane-grill-0853158p.html#srp",
+  "https://www.canadiantire.ca/en/pdp/weber-spirit-3-burner-propane-bbq-0853196p.html#srp",
+  "https://www.canadiantire.ca/en/pdp/coleman-cookout-4-burner-propane-bbq-0853182p.html#srp",
+  "https://www.canadiantire.ca/en/pdp/napoleon-ld3-3-burner-natural-gas-grill-0853159p.html#srp",
+  "https://www.canadiantire.ca/en/pdp/stanley-black-chrome-socket-set-183-pc-0589287p.html",
+  "https://www.canadiantire.ca/en/pdp/dewalt-dcf887b-20v-max-xr-brushless-3-speed-impact-driver-tool-only-0546726p.html#srp",
+  "https://www.canadiantire.ca/en/pdp/dewalt-dcb203-2-20v-max-2-0-ah-li-ion-battery-2-pk-0543201p.html#srp",
+  "https://www.canadiantire.ca/en/pdp/sodastream-fizzi-one-touch-sparkling-water-maker-black-0431019p.html",
+  "https://www.canadiantire.ca/en/pdp/chamberlain-medium-lift-belt-drive-garage-door-opener-with-wi-fi-0460594p.html"
+  
+)
+
+scrape_canadian_tire <- function(product_url_list) {
+  lapply(product_url_list, function(x) {
+    scrape_url(x)
+    Sys.sleep(3)
+  })  
+}
