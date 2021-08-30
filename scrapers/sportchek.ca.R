f9573232083aaa070569cdb6ee3f438af9e38bf4
Cameron <cwillmot@gmail.com>
2021-08-30 16:14:03 -0400

    create sportchek scraper based on canadiantire

---
+++ scrapers/sportchek.ca.R
@@ -0,0 +1,61 @@
+################################################################################
+# Script to scrape from sportchek.ca
+################################################################################
+
+# Load required packages
+packagesNeeded <- c('tidyverse', 'rvest', "R.utils", 'httr')
+package.check <- lapply(
+  packagesNeeded,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+# Source the saving script for later use
+source('dbsave.R')
+
+
+scrape_url <- function(url) {
+  system(paste("C:\\Users\\Cam\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe scrapers\\sportchek.ca.js",url))
+  product_html <- read_html('sc.html')
+  
+  product_price <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-sale-price", " " ))]') %>% html_text()
+  
+  if (length(product_price) == 0) {
+    product_price <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail__price-text", " " ))]') %>% html_text()
+    
+  } else {
+    product_price <- product_price[1]
+  }
+  
+  product_name <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "global-page-header__title", " " ))]') %>% html_text() %>% str_remove_all("\n")
+  product_code <- product_html %>% html_nodes(xpath='//*[(@id = "product-detail__description-style-num")]') %>% html_text()
+  product_code <- product_code[1]
+  
+  product <- tibble(
+    name = product_name,
+    price = product_price,
+    code = product_code,
+    request_url = url,
+    date_scraped = Sys.time()
+  )
+  save_scrape(product) 
+}
+
+product_url_list <- c(
+  "https://www.sportchek.ca/categories/women/footwear/sneakers-lifestyle-boots/sneakers/product/converse-chuck-hi-shoes-black-white-unisex-331444675.html",
+  "https://www.sportchek.ca/categories/women/mothers-day-gift-guide/activewear/product/apple-airpods-with-charging-case-color-332944799_10-332944799.html",
+  "https://www.sportchek.ca/categories/women/footwear/running-shoes/neutral/product/asics-womens-gel-excite-8-running-shoes-color-333371880_01-333371880.html",
+  "https://www.sportchek.ca/categories/shop-by-sport/running/electronics/product/garmin-fenix-6s-pro-solar-fitness-watch-color-333333997_54-333333997.html"
+)
+
+scrape_sportchek <- function(product_url_list) {
+  lapply(product_url_list, function(x) {
+    scrape_url(x)
+    Sys.sleep(3)
+  })  
+}
+scrape_sportchek(product_url_list)
