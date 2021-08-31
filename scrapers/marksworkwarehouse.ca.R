5a99f9c1a28e56bc5360627b713682e3e6b588f9
Cameron <cwillmot@gmail.com>
2021-08-31 15:12:12 -0400

    refactored price scrape to return numeric.  refactored name and code to clean up output

--- scrapers/marksworkwarehouse.ca.R
+++ scrapers/marksworkwarehouse.ca.R
@@ -22,18 +22,35 @@ scrape_url <- function(url) {
   system(paste("C:\\Users\\Cam\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe scrapers\\marksworkwarehouse.ca.js",url))
   product_html <- read_html('mww.html')
   
-  product_price <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail-price__current", " " ))]') %>% html_text()
+  product_price <- product_html %>% 
+    html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail-price__current", " " ))]') %>% 
+    html_text() %>%
+    str_extract_all(pattern="([0123456789.]+,*[0123456789.]*)") %>% 
+    unlist() %>% 
+    gsub(pattern=",", replacement="") %>%
+    as.numeric() %>%
+    min()
   
-  if (length(product_price) == 0) {
-    # product_price <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail__price-text", " " ))]') %>% html_text()
-    print(paste("Price error on url", url, " price recorded is: ", product_price))
-  } else {
-    product_price <- product_price[1]
-  }
+  #if (length(product_price) == 0) {
+  #  # product_price <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail__price-text", " " ))]') %>% html_text()
+  #  print(paste("Price error on url", url, " price recorded is: ", product_price))
+  #} else {
+  #  product_price <- product_price[1]
+  #}
   
-  product_name <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail-info__title", " " ))]') %>% html_text() %>% str_remove_all("\n")
-  product_code <- product_html %>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail-overview__style", " " ))]') %>% html_text()
-  product_code <- product_code[1]
+  product_name <- product_html %>% 
+    html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail-info__title", " " ))]') %>% 
+    html_text() %>% 
+    str_remove_all("\n")
+    
+  
+  product_code <- product_html %>% 
+    html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "product-detail-overview__style", " " ))]') %>% 
+    html_text() %>%
+    gsub(pattern="\\n",replacement="") %>%
+    gsub(pattern="Style: ", replacement="")
+  
+  # product_code <- product_code[1]
   
   product <- tibble(
     name = product_name,
@@ -42,6 +59,7 @@ scrape_url <- function(url) {
     request_url = url,
     date_scraped = Sys.time()
   )
+  product
   save_scrape(product) 
 }
 
@@ -49,7 +67,11 @@ product_url_list <- c(
   "https://www.marks.com/en/denver-hayes-mens-long-sleeve-classic-fit-core-casual-shirt-color-ltgrey-336632.html",
   "https://www.marks.com/en/denver-hayes-mens-fleece-hoodie-sweatshirt-color-stohea-315330.html",
   "https://www.marks.com/en/dakota-mens-trifold-wallet-with-id-window-color-black-302211.html",
-  "https://www.marks.com/en/denver-hayes-mens-short-sleeve-modern-fit-core-casual-shirt-color-navy-345532.html"
+  "https://www.marks.com/en/denver-hayes-mens-short-sleeve-modern-fit-core-casual-shirt-color-navy-345532.html",
+  "https://www.marks.com/en/saucony-mens-grid-marauder-3-running-shoes-grey-color-grey-346259.html",
+  "https://www.marks.com/en/scrubletics-womens-v-neck-athletic-kangaroo-pocket-scrub-top-black-color-black-323066.html",
+  "https://www.marks.com/en/carhartt-mens-athletic-cargo-scrub-pants-color-pewter-239046.html",
+  "https://www.marks.com/en/timberland-mens-logan-bay-leather-chukka-boots-brown-color-brown-103256.html"
 )
 
 scrape_marks <- function(product_url_list) {
