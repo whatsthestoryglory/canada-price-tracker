c317c80a7c99b590cab6f2925dae06c602a5266f
Cameron <cwillmot@gmail.com>
2021-09-01 10:08:42 -0400

    update product table when saving scrapes

--- dbsave.R
+++ dbsave.R
@@ -30,10 +30,65 @@ price_collection <- mongo(collection="prices", db="priceScrapeResults", url=conn
 
 #price_collection$insert(product)
 
-save_scrape <- function(product) {
-  result <- price_collection$insert(product)
+save_scrape <- function(scrape) {
+  result <- price_collection$insert(scrape)
   if (length(result$writeErrors) > 0) {
-    print("db write error")
+    print("scrape db write error")
     print(result$writeErrors) 
+  } else { print("wrote scrape to db") }
+  
+  # Determine price aggregates for product
+  db_result <- product_collection$find(paste0('{"url" : "',scrape$request_url,'"}'))
+  
+  # Determine max price
+  highest_product_price <- max(
+    db_result$highest_price,
+    scrape$price
+  )
+  
+  # Determine min price
+  lowest_product_price <- min(
+    db_result$lowest_price,
+    scrape$price
+  )
+  
+  # Determine typical price
+  # Define typical price as the most frequent price observed
+  getmode <- function(v) {
+    as.numeric(names(sort(-table(v)))[1])
+  }
+  
+  typical_product_price <- getmode(
+    price_collection$find(paste0(
+      '{"request_url" : "',scrape$request_url,'"}'),
+      '{"price":1,"_id":0}')
+    # Future update add qualifier to only return scrapes from last 90 days
+  )
+  
+  product <- tibble(
+    product_name = scrape$name,
+    last_scraped = scrape$date_scraped,
+    last_price = scrape$price,
+    highest_price = highest_product_price,
+    lowest_price = lowest_product_price,
+    typical_price = typical_product_price
+  )
+  
+  result <- product_collection$update(
+    sprintf('{"url":"%s"}', scrape$request_url),
+    sprintf('{ "$set": { "product_name" : "%s", "last_scraped" : "%s", "last_price" : "%s", "highest_price" : "%s", "lowest_price" : "%s", "typical_price" : "%s" } }',
+            product$product_name,
+            product$last_scraped,
+            product$last_price,
+            product$highest_price,
+            product$lowest_price,
+            product$typical_price)
+  )
+  
+  if (length(result$writeErrors) > 0) {
+    print("product db write error")
+    print(result$writeErrors) 
+  } else {
+    print("product db updated successfully")
   }
 }
