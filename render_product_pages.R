35391403c6328f5a9e57f17fcfdf4f18ef1a7e39
Cameron <cwillmot@gmail.com>
2021-09-02 14:17:52 -0400

    create script to render all product pages when called

---
+++ render_product_pages.R
@@ -0,0 +1,24 @@
+################################################################################
+# Script to render all product pages
+################################################################################
+
+# Source the products script for later use
+source('products.R')
+
+product_urls <- product_collection$distinct("url")
+
+render_product_page <- function(url) {
+  domain <- urltools::suffix_extract(urltools::domain(url))$domain
+  page <- urltools::url_parse(url)$path
+  page <- unlist(strsplit(page, "/"))
+  page <- page[length(page)]
+  rmarkdown::render(
+    "product_page.Rmd", 
+    params = list(
+      url = url
+    ),
+    output_file = paste0("products\\",domain,"\\",page)
+  )
+}
+
+result <- lapply(product_urls, render_product_page())
